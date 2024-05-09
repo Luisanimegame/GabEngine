e0bbb20ab2fe68c0bc7824f7145eddbb57935a24
Faker_wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2024-05-09 00:19:19 -0300

    Odio

--- source/OthersState.hx
+++ source/OthersState.hx
@@ -142,8 +142,15 @@ class OthersState extends MusicBeatState
 		super.create();
 	}
 
+	//#if ACHIEVEMENTS_ALLOWED
+	// Unlocks "Freaky on a Friday Night" achievement
+	function giveAchievement() {
+		//add(new AchievementObject('friday_night_play', camAchievement));
+		//FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+		//trace('Giving achievement "friday_night_play"');
 	}
-	
+	#end
+
 	var selectedSomethin:Bool = false;
 
 	override function update(elapsed:Float)

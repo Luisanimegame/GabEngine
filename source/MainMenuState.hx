598facced2811584b2490e31b46fc11e116dd132
Faker_wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2024-05-08 22:57:15 -0300

    Odeio my life

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -113,7 +113,7 @@ class MainMenuState extends MusicBeatState
 		for (i in 0...optionShit.length)
 		{
 			var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
-			var menuItem:FlxSprite = new FlxSprite(-80));
+			var menuItem:FlxSprite = new FlxSprite(-80);
 			menuItem.frames = Paths.getSparrowAtlas('menuz/huh-' + optionShit[i]);
 			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);

d3700663ce4a2035502e769a98651b2cdec0a84f
Faker_wuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2024-06-05 01:25:57 -0300

    I love my live

--- source/FreeplayAteuwuState.hx
+++ source/FreeplayAteuwuState.hx
@@ -43,18 +43,10 @@ class FlashingState extends MusicBeatState
 		if(!leftState) {
 			var back:Bool = controls.BACK;
 			if (controls.ACCEPT || back) {
-				new FlxTimer().start(0.5, function (tmr:FlxTimer) {
 							MusicBeatState.switchState(new FreeplayState());
 				if(!back) {
-						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
 							MusicBeatState.switchState(new ExtraState());
 						});
-				} else {
-					FlxG.sound.play(Paths.sound('cancelMenu'));
-					FlxTween.tween(warnText, {alpha: 0}, 1, {
-						onComplete: function (twn:FlxTween) {
-							MusicBeatState.switchState(new TitleState());
-						}
 					});
 				}
 			}
